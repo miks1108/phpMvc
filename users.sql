2eaab82bbb163467e4fbe406e56d417c9093b343
Вадим <guts.vadim@gmail.com>
2015-10-27 19:12:29 +0200

    users.sql

---
+++ users.sql
@@ -0,0 +1,52 @@
+CREATE TABLE `users` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `UserName` varchar(50) NOT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `unique_UserName` (`UserName`)
+) ENGINE=InnoDB AUTO_INCREMENT=83 DEFAULT CHARSET=utf8;
+
+INSERT INTO itstep_db.users (Id, UserName) VALUES (11, 'ad');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (30, 'adipisicing');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (67, 'aliqua');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (49, 'aliquip');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (36, 'amet');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (34, 'aute');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (20, 'cillum');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (66, 'commodo');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (54, 'consectetur');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (70, 'culpa');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (32, 'cupidatat');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (78, 'deserunt');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (75, 'do');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (76, 'dolor');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (72, 'dolore');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (14, 'duis');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (7, 'ea');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (37, 'elit');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (13, 'esse');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (21, 'et');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (24, 'ex');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (18, 'excepteur');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (42, 'id');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (27, 'in');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (45, 'incididunt');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (12, 'ipsum');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (71, 'irure');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (40, 'laboris');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (57, 'laborum');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (9, 'magna');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (6, 'minim');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (41, 'nulla');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (81, 'occaecat');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (69, 'pariatur');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (62, 'qui');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (17, 'quis');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (63, 'reprehenderit');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (31, 'sint');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (55, 'sit');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (22, 'sunt');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (59, 'tempor');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (35, 'ullamco');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (29, 'ut');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (74, 'velit');
+INSERT INTO itstep_db.users (Id, UserName) VALUES (64, 'veniam');
