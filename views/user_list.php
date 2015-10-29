5daa477b320c71ea617946e80abd0585a8685c99
Вадим <guts.vadim@gmail.com>
2015-10-29 21:12:27 +0200

    pagination

--- views/user_list.php
+++ views/user_list.php
@@ -7,10 +7,10 @@
         </li>
     <? } ?>
 </ul>
+<br>
 
-<a href="?page=1">1</a>
-<a href="?page=2">2</a>
-<a href="?page=3">3</a>
-<a href="?page=4">4</a>
-<a href="?page=5">5</a>
-<a href="?page=6">6</a>
+<? for($i = 1; $i <= $this->pageCount; $i++) { ?>
+    <a href="?page=<?=$i?>">
+        [<?=$i?>]
+    </a>
+<? } ?>
