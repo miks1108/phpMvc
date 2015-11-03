7a1fa2ea3b47d8c3ff7928a9854a47cf7f167be3
Вадим <guts.vadim@gmail.com>
2015-11-03 20:44:55 +0200

    register form values added

--- views/register.php
+++ views/register.php
@@ -1,20 +1,22 @@
 <h1>Register new user</h1>
 
-<div class="errors">
-    <? foreach ($this->erros as $error) { ?>
-        <p><?=$error?></p>
-    <? } ?>
-</div>
+<? if(!empty($this->errors)) { ?>
+    <div class="errors">
+        <? foreach ($this->errors as $error) { ?>
+            <p><?=$error?></p>
+        <? } ?>
+    </div>
+<? } ?>
 
 <form action="" method="post">
     User Name:
-    <input type="text" name="userName"> <br>
+    <input type="text" name="userName" value="<?=$_POST['userName']?>"> <br>
     Email:
-    <input type="text" name="email"> <br>
+    <input type="text" name="email" value="<?=$_POST['email']?>"> <br>
     Password:
-    <input type="text" name="password"> <br>
+    <input type="text" name="password" value="<?=$_POST['password']?>"> <br>
     Confirm password:
-    <input type="text" name="cpassword"> <br>
+    <input type="text" name="cpassword" value="<?=$_POST['cpassword']?>"> <br>
 
     <input type="submit" value="Register"> <br>
 </form>
